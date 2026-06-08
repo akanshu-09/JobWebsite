d339a0135a87e86b358ac9c99d977dba519c53f9
akanshu-09 <akanshuy009@gmail.com>
2026-06-08 15:12:33 +0530

    Changed packaging type to War files and extended SpringBootServletInitializer in main.

--- src/main/java/org/project/jobwebsite/JobwebsiteApplication.java
+++ src/main/java/org/project/jobwebsite/JobwebsiteApplication.java
@@ -2,11 +2,11 @@ package org.project.jobwebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JobwebsiteApplication
+public class JobwebsiteApplication extends SpringBootServletInitializer
 {
-
     public static void main(String[] args) {
         SpringApplication.run(JobwebsiteApplication.class, args);
     }
